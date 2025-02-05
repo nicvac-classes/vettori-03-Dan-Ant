cd42ebe56ac9968984bd489b9ba83f2db151644c
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2025-02-05 15:18:14 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,56 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+float n, i, ind;
+cout << "Quanti studenti ci sono in classe?" << endl;
+cin >> n;
+string nomi[n];
+float voti[n];
+float v;
+i=0;
+while (i<n)
+{
+    cout << "Qual è il tuo nome?" << endl;
+    cin >> nomi[i];
+    cout << "Qual è il tuo voto?" << endl;
+    cin >> voti [i];
+    if(i=0)
+    {
+        v=voti[i];
+    }
+    else 
+    {
+        if (v<voti[i])
+        {
+            v=voti[i];
+        }
+    }
+    i=i+1;
+}
+cout << "I datii della classe sono i seguenti: " << endl;
+i=0;
+while (i<n)
+
+{
+ cout << "Nome: " << nomi[i] << " voto: " << voti[i];
+ i=i+1;
+}
+i=0;
+while (i<n)
+{
+    if (voti[i]=v)
+    {
+        ind=i;
+    }
+    i=i+1;
+}
+if(ind>=0)
+{"Nome: " << nomi[ind] <<
+" voto: " << voti[ind];
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+
+}
+
+
